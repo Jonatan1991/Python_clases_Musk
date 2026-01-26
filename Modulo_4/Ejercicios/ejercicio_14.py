6cb9c76b838cecb4a09b9b7a92adfa0efffc3a6d
Jonatan1991 <75802929+Jonatan1991@users.noreply.github.com>
2026-01-26 23:35:19 +0100

    Añadir programa para seleccionar todos los domingos de un año determinado

---
+++ Modulo_4/Ejercicios/ejercicio_14.py
@@ -0,0 +1,45 @@
+# Escribe un programa en Python para seleccionar
+# todos los domingos de un año determinado.
+
+
+from datetime import datetime, timedelta
+
+def domingos_anno(anno):
+
+    domingos = []
+    fecha = datetime(anno, 1, 1)
+    print(anno)
+    print(fecha.strftime("%Y"))
+    print(fecha.strftime("%A"))
+
+    while fecha.strftime("%Y") == str(anno):
+        fecha = fecha + timedelta(days = 1)
+        if fecha.strftime("%A") == "Sunday":
+            print(fecha.strftime("%A %d %B %Y"))
+
+
+# domingos_anno(2025)
+
+
+# Otra forma, pero con menos iteraciones:
+
+def domingos_x_anno(anno):
+    fecha = datetime(anno, 1, 1)
+
+    while fecha.strftime("%A") != "Sunday":
+        fecha = fecha + timedelta(days=1)
+        
+
+    print(fecha.strftime("%A %d %B %Y"))
+    while fecha.year == anno:
+        fecha = fecha + timedelta(days=7)
+        if fecha.year == anno:
+            print(fecha.strftime("%A %d %B %Y"))
+
+            
+
+domingos_x_anno(2026)
+        
+
+
+
